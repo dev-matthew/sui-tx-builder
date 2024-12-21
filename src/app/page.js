c9742810c34032607ae9a76a1996e4fc47c347ea
Mach Solutions <m_fogel4@yahoo.com>
2024-12-21 00:24:46 -0800

    Setup ReactFlow canvas

--- src/app/page.js
+++ src/app/page.js
@@ -4,16 +4,24 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Toaster } from "@/components/ui/toaster";
 
+import { ReactFlow, Background, Controls } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+
 export default function Main() {
   return (
     <>
       <SidebarProvider
           style={{
-            "--sidebar-width": "24rem"
+            "--sidebar-width": "26rem" // Adjust sidebar width
           }}
         >
         <AppSidebar />
-        <SidebarTrigger className="ml-4 mt-4"/>
+        <div className="w-full h-screen">
+          <ReactFlow>
+            <Background />
+            <Controls />
+          </ReactFlow>
+        </div>
       </SidebarProvider>
       <Toaster />
     </>
