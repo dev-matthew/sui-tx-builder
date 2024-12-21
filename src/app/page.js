07061e8dbbbf6ec797c6e26ebd43a0d9fee32337
Mach Solutions <m_fogel4@yahoo.com>
2024-12-20 23:54:29 -0800

    Create sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,52 +1,8 @@
-"use client";
-
-import { useState } from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  const [transactionDigest, setTransactionDigest] = useState("");
-
-  const handleInputChange = (event) => {
-    setTransactionDigest(event.target.value);
-  };
-
-  const handleSearchClick = async () => {
-    try {
-      const response = await fetch("/api/search", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ transactionDigest }),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-  
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      handleSearchClick();
-    }
-  };
 
   return (
-    <div className="flex w-full max-w-sm items-center space-x-2">
-      <Input
-          placeholder="Enter transaction digest..."
-          value={transactionDigest}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-      />
-      <Button type="submit" onClick={handleSearchClick}>
-          Search
-      </Button>
-    </div>
+    <>
+      
+    </>
   );
 }
---
+++ src/components/app-sidebar.js
@@ -0,0 +1,104 @@
+"use client";
+
+import { useState } from "react";
+
+import { Github, Link } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarFooter
+} from "@/components/ui/sidebar";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/hooks/use-toast";
+import { Separator } from "@/components/ui/separator"
+ 
+export function AppSidebar() {
+  const [transactionDigest, setTransactionDigest] = useState("");
+  const { toast } = useToast()
+
+  const handleInputChange = (event) => {
+    setTransactionDigest(event.target.value);
+  };
+
+  const handleSearchClick = async () => {
+    try {
+      const response = await fetch("/api/search", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ transactionDigest }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      console.log(data);
+
+      if (data.result == null) {
+        throw new Error(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      toast({
+        title: `Couldn't find ${transactionDigest}`,
+        description: error.message,
+        variant: "destructive"
+      })
+    }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearchClick();
+    }
+  };
+
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <SidebarGroupLabel className="font-bold text-2xl text-gray-900 mt-4">Sui Transaction Builder</SidebarGroupLabel>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Inspect Existing Transactions</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <div className="flex w-full max-w-sm items-center space-x-2">
+              <Input
+                placeholder="Enter transaction digest..."
+                value={transactionDigest}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+                className="ml-2"
+              />
+              <Button
+                type="submit"
+                onClick={handleSearchClick}
+              >
+                Search
+              </Button>
+            </div>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Basic Building Blocks (Drag and Drop)</SidebarGroupLabel>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupLabel>Templates (Drag and Drop)</SidebarGroupLabel>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <div className="flex h-5 items-center space-x-4 text-sm justify-between w-full">
+          <SidebarGroupLabel><a href="https://github.com/dev-matthew/sui-tx-builder" target="_blank" className="flex items-center"><Github className="mr-2"></Github><b>Source Code</b></a></SidebarGroupLabel>
+          <Separator orientation="vertical" />
+          <SidebarGroupLabel>Data provided by the&nbsp;<a href="https://blockberry.com" target="_blank"><b>Blockberry API</b></a></SidebarGroupLabel>
+        </div>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
